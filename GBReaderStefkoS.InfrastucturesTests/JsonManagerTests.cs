0bb30e16d1d67736b7775f837daa581f6ca2908d
Q210020 <s.stefko@student.helmo.be>
2022-12-23 23:50:24 +0100

    version finale v2

--- GBReaderStefkoS.InfrastucturesTests/JsonManagerTests.cs
+++ GBReaderStefkoS.InfrastucturesTests/JsonManagerTests.cs
@@ -14,7 +14,7 @@ public class JsonManagerTests
     private static readonly char _separator = Path.DirectorySeparatorChar;
     private static readonly string _directoryPath = _userPath + _separator + "ue36";
     
-    private static string _jsonFileNotExist = "q210020-not-exist.json";
+    private static string _jsonFieTest = "q210020-test.json";
     
     //[SetUp]
     //public void Setup()
@@ -26,26 +26,30 @@ public class JsonManagerTests
     [Test]
     public void JsonNotExist()
     {
-         ISessionRepository sessionRepository = new SessionRepository(_jsonFileNotExist);
-         Assert.That(File.Exists(_directoryPath + _separator + _jsonFileNotExist), Is.EqualTo(true));
+         ISessionRepository sessionRepository = new SessionRepository(_jsonFieTest);
+         Assert.That(File.Exists(_directoryPath + _separator + _jsonFieTest), Is.EqualTo(true));
 
          sessionRepository.LoadSessions();
          
-         Assert.That(File.Exists(_directoryPath + _separator + _jsonFileNotExist), Is.EqualTo(true));
+         Assert.That(File.Exists(_directoryPath + _separator + _jsonFieTest), Is.EqualTo(true));
+         
+         File.Delete(_directoryPath + _separator + _jsonFieTest);
     }
     
     // si des arguments sont manquants, les sessions auquelles ils manquent des artuments sont ignorées
     [Test]
     public void JsonArgReadingSessionMissing()
     {
-        ISessionRepository sessionRepository = new SessionRepository(_jsonFileNotExist);
+        ISessionRepository sessionRepository = new SessionRepository(_jsonFieTest);
         string json = "[{'BookTitle':'stevy en anglais','BookIsbn':,'PageIndex':3,'DateBeginning':'23/12/2022 23:11:58','DateLastReading':'23/12/2022 23:12:05'}]";
         
-        writeToFile(_directoryPath + _separator + _jsonFileNotExist, json);
+        writeToFile(_directoryPath + _separator + _jsonFieTest, json);
         
         var sessions = sessionRepository.LoadSessions();
         
         Assert.That(sessions.Count, Is.EqualTo(0));
+        
+        File.Delete(_directoryPath + _separator + _jsonFieTest);
         
     }
 
@@ -53,16 +57,18 @@ public class JsonManagerTests
     [Test]
     public void JsonNotWellFormated()
     {
-        ISessionRepository sessionRepository = new SessionRepository(_jsonFileNotExist);
+        ISessionRepository sessionRepository = new SessionRepository(_jsonFieTest);
         string json = "[{'BookTitle';'stevy en anglais','BookIsbn':,'PageIndex':3,'DateBeginning':'23/12/2022 23:11:58','DateLastReading':'23/12/2022 23:12:05'}]";
         
-        writeToFile(_directoryPath + _separator + _jsonFileNotExist, json);
+        writeToFile(_directoryPath + _separator + _jsonFieTest, json);
         
-        Assert.That(new FileInfo(_directoryPath + _separator + _jsonFileNotExist).Length > 0, Is.EqualTo(true));
+        Assert.That(new FileInfo(_directoryPath + _separator + _jsonFieTest).Length > 0, Is.EqualTo(true));
         
         var sessions = sessionRepository.LoadSessions();
         
-        Assert.That(new FileInfo(_directoryPath + _separator + _jsonFileNotExist).Length > 0, Is.EqualTo(false));
+        Assert.That(new FileInfo(_directoryPath + _separator + _jsonFieTest).Length > 0, Is.EqualTo(false));
+        
+        File.Delete(_directoryPath + _separator + _jsonFieTest);
     }
     
     private void writeToFile(string fileName, string content)
