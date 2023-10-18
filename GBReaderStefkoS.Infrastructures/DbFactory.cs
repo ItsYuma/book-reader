e1fb72eacd7d5f8321ad260c598ab70b0e8abcde
Yuma <116975265+ItsYuma@users.noreply.github.com>
2023-10-18 21:48:44 +0200

    connexion string

--- GBReaderStefkoS.Infrastructures/DbFactory.cs
+++ GBReaderStefkoS.Infrastructures/DbFactory.cs
@@ -9,7 +9,7 @@ namespace GBReaderStefkoS.Infrastructures
     public class DbFactory
     {
         private const string ProviderName = "MySql.Data.MySqlClient";
-        private const string DbConnectionString = "Server=192.168.132.200;" + "Port=13306;" + "Database=Q210020;" + "uid=Q210020;" + "pwd=0020;";
+        private const string DbConnectionString = "ConnexionString";
         
         private readonly DbProviderFactory _factory;
         //private const string DbConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\utilisateur\OneDrive - student.helmo.be\CursusInfo\Bloc2\StructuresDeDonnees\22-23\ExerciceTheorie\C#\AdoDotNet\Helmo.Sd.Theory\Helmo.Sd.Theory.Ado.Tests\DBTests.mdf"";Integrated Security=True";
